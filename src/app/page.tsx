1c82b05a173d80f1acd99cb4c60fb56719637341
tishibashi1003 <tetsuro.ishibashi@kikagaku.co.jp>
2025-01-30 15:27:06 +0900

    refactor: Move SwipeScreen to search page and update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import SwipeScreen from '@/features/routes/event/components/swipe-screen';
-
 export default function Home() {
-  return <SwipeScreen />;
+  return <div>home</div>;
 }
---
+++ src/app/search/page.tsx
@@ -0,0 +1,5 @@
+import SwipeScreen from '@/features/routes/event/components/swipe-screen';
+
+export default function SearchPage() {
+  return <SwipeScreen />;
+}
