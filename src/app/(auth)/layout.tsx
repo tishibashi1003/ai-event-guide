01b3b14b9a2d9eac220597384677b58fdb3ba1f3
tishibashi1003 <tetsuro.ishibashi@kikagaku.co.jp>
2025-02-05 11:33:02 +0900

    refactor: Simplify auth and landing page layouts by removing redundant HTML structure

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,8 @@
 import Layout from '@/features/common/layout/layout';
 import './../globals.css';
-import { Inter } from 'next/font/google';
 import type React from 'react';
 import { AuthGuard } from '@/features/common/auth/AuthGuard';
 import { Toaster } from '@/components/ui/toast/toaster';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'ココいく',
@@ -18,13 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='ja'>
-      <body className={inter.className}>
-        <Layout currentPath={'/'}>
-          <AuthGuard>{children}</AuthGuard>
-        </Layout>
-        <Toaster />
-      </body>
-    </html>
+    <Layout currentPath={'/'}>
+      <AuthGuard>{children}</AuthGuard>
+      <Toaster />
+    </Layout>
   );
 }
